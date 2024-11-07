074d1639748e203ae125ef9f9ce08e4d40318ebe
Darsh1153 <161435139+Darsh1153@users.noreply.github.com>
2024-11-07 07:37:50 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accId = 123;
+let accEmail = "darsh@gmail.com"
+var accPassword = "13456"
+accCity = "Jaipur"
+
+// accId = 1234;
+console.log(accId);
+
+console.table([accId, accEmail, accPassword, accCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JS code as newer version
+
+let name = "Darshan Sharma"
+let age = 22
+let isValid = true
+
+/*
+------Data Types--------
+number -> 2 to the power 53
+bigInt
+String => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique
+*/
+
+// object
+console.log(typeof "Darshan Sharma")
