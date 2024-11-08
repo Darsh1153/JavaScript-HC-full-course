3fe3fa4b49c5718839a30b3f279a47cb744b6402
Darsh1153 <161435139+Darsh1153@users.noreply.github.com>
2024-11-08 07:19:22 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,25 @@
+// -----------array--------------
+const myArr = [0,1,2,3,4]
+// console.log(myArr)
+
+myArr.push(6)
+// console.log(myArr)
+
+
+myArr.pop()
+// console.log(myArr)
+
+
+// myArr.unshift(9)
+// console.log(myArr)
+
+myArr.shift()
+// console.log(myArr)
+
+
+// slice and splice
+// slice
+const newArr = myArr;
+console.log(newArr.slice(1,3))
+
+console.log(newArr.splice(1,3)) // removes that particular portion
