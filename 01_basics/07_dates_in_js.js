e6a77a6ef72cef47710ba8b0a88627a11e70abf7
Darsh1153 <161435139+Darsh1153@users.noreply.github.com>
2024-11-08 06:48:23 +0000

    dates and timestamps

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,11 @@
+// Dates
+let myDate = new Date();
+// console.log(myDate);
+// console.log(typeof myDate);
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+
+// ------TimeStamp------
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+
